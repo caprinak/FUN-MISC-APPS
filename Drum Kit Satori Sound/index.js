6f50107fc9fbee6b974cf84998a4631b1a695a35
khoana <khoacoyotes.na@gmail.com>
2025-04-20 11:37:40 +0700

    first commit

---
+++ Drum Kit Satori Sound/index.js
@@ -0,0 +1,81 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
+}
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var sat1 = new Audio("sounds/sat-1.mp3");
+      sat1.play();
+      break;
+
+    case "a":
+      var sat2 = new Audio("sounds/sat-2.mp3");
+      sat2.play();
+      break;
+
+    case "s":
+      var sat3 = new Audio('sounds/sat-3.mp3');
+      sat3.play();
+      break;
+
+    case "d":
+      var sat4 = new Audio('sounds/sat-4.mp3');
+      sat4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
